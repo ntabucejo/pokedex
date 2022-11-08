635cd2b03abd44e230c01a07b55d7c0272f46daa
ntabucejo <ntabucejo@gmail.com>
2022-11-09 00:07:32 +0800

    fix: add viewport meta

--- app/head.tsx
+++ app/head.tsx
@@ -4,6 +4,7 @@ const Head = () => {
       <title>Pokedex</title>
       <meta name="description" content="A Pokemon Collection" />
       <link rel="icon" href="/favicon.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </>
   );
 };
