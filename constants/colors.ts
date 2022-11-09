656b60a98f28f7a283897c44c6626aa62a107e12
ntabucejo <ntabucejo@gmail.com>
2022-11-09 15:45:51 +0800

    chore: add constants for colors

---
+++ constants/colors.ts
@@ -0,0 +1,24 @@
+const colors = {
+  type: {
+    normal: "#929BA3",
+    fire: "#FF983F",
+    water: "#3393DD",
+    grass: "#35C04A",
+    flying: "#8AACE4",
+    fighting: "#E12C6A",
+    poison: "#B667CF",
+    electric: "#FBD200",
+    ground: "#E97333",
+    rock: "#C9B787",
+    psychic: "#FF6676",
+    ice: "#4BD2C1",
+    bug: "#84C400",
+    ghost: "#4B6AB3",
+    steel: "#5A8FA3",
+    dragon: "#0070CA",
+    dark: "#5B5366",
+    fairy: "#FB8AEC",
+  },
+};
+
+export default colors;
