f200a53db8ce40b1660c51ae2b20cd6a44569c38
ntabucejo <ntabucejo@gmail.com>
2022-11-09 15:47:32 +0800

    chore: add image domain

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: { appDir: true },
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ["www.professorlotus.com"],
+  },
 };
 
 module.exports = nextConfig;
